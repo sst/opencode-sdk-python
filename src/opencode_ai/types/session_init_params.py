c914b7bb98cc56472669f57172af339bd189cab2
Wang ChengMeng <79295644+cmw-creator@users.noreply.github.com>
2026-06-06 12:39:08 +0000

    fix: nest model_id/provider_id under model key in SessionInitParams

--- src/opencode_ai/types/session_init_params.py
+++ src/opencode_ai/types/session_init_params.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from typing_extensions import Required, Annotated, TypedDict
 
 from .._utils import PropertyInfo
+from .session_chat_params import ModelParam
 
 __all__ = ["SessionInitParams"]
 
@@ -12,6 +13,4 @@ __all__ = ["SessionInitParams"]
 class SessionInitParams(TypedDict, total=False):
     message_id: Required[Annotated[str, PropertyInfo(alias="messageID")]]
 
-    model_id: Required[Annotated[str, PropertyInfo(alias="modelID")]]
-
-    provider_id: Required[Annotated[str, PropertyInfo(alias="providerID")]]
+    model: Required[ModelParam]
